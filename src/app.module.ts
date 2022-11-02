4d451d3c3ffe9bc6efbc33c0a8395525c0ed3faf
Chanhui Lee <chanhuil@c2r10s3.42seoul.kr>
2022-11-02 17:32:46 +0900

    add: TypeOrm basic settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
