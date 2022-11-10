815b37275adae3867acb3385c666963a83d193de
Chanhui Lee <chanhuil@c3r9s5.42seoul.kr>
2022-11-10 14:11:15 +0900

    image upload prototype

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger, Post, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { existsSync, mkdirSync } from 'fs';
+import { diskStorage } from 'multer';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +12,26 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file',
+  {
+    storage: diskStorage({
+      destination: (req, file, cb) => {
+        const uploadPath: string = 'upload';
+        if (!existsSync(uploadPath)) {
+          mkdirSync(uploadPath);
+        }
+        cb(null, uploadPath);
+      },
+      filename: (req, file, cb) => {
+        cb(null, file.originalname);
+      }
+    })
+  }))
+  uploadFile() {
+    const logger = new Logger(AppController.name);
+    logger.log('File Uploaded!');
+  }
+
 }
