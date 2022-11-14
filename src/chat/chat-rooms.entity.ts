9f1fe2a209a9363711f9236396b3ed0c42739479
AYoungSn <dudtjs0920@naver.com>
2022-11-14 14:38:57 +0900

    #6 chat room entity 생성

---
+++ src/chat/chat-rooms.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('ChatRoom')
+export class ChatRoomsEntity {
+	@PrimaryGeneratedColumn('rowid')
+	id: number;
+
+	@Column()
+	owner_id: number;
+
+	@Column()
+	name: string;
+
+	@Column()
+	access_modifier: string;
+
+	@Column()
+	password: string;
+
+	@Column()
+	create_date: Date;
+
+	@Column()
+	update_date: Date;
+};
