421b60005f2d550fb32bb24cf82ada106d7d4c29
AYoungSn <dudtjs0920@naver.com>
2022-11-24 21:15:37 +0900

    [add] #14 channel 전체 조회, 참여한 채널 조회 API

---
+++ src/chat/channels.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserService } from "src/user/user.service";
+import { Repository } from "typeorm";
+import { ChatRoomsEntity } from "./entity/chat-rooms.entity";
+import { RoomUserInfoEntity } from "./entity/RoomUserInfo.entity";
+
+@Injectable()
+export class ChannelService {
+	constructor(
+		@InjectRepository(ChatRoomsEntity) private chatRoomsRepository: Repository<ChatRoomsEntity>,
+		@InjectRepository(RoomUserInfoEntity) private roomUserInfoRepository: Repository<RoomUserInfoEntity>,
+		private userService: UserService,
+	) {}
+
+	async getAllChannels(headers: any) {
+		const user = await this.userService.findByUser(headers.authorization);
+		/* select id, owner_id, access_modifier, name from chat-rooms cr, RoomUserInfo rinfo
+		   where cr.id  */
+		const channels = (await this.chatRoomsRepository.find());
+		const ret = []
+		for (let ch in channels) {
+			let roominfo = await this.roomUserInfoRepository.findOneBy({id: channels[ch].id, user_id: user.id})
+			if (roominfo.ban == true) {
+				continue
+			}
+			ret.push({
+				room_id: channels[ch].id,
+				name: channels[ch].name,
+				access_modifier: channels[ch].access_modifier
+			})
+		}
+		return ret;
+	}
+
+	async getParticipateChannels(headers: any, intra_id: string) {
+		// header 체크
+		const user = await this.userService.findByUser(headers.authorization);
+		if (user.intra_id == intra_id) {
+			const rooms = (await this.roomUserInfoRepository.findBy({user_id: user.id}));
+			const ret = []
+			for(let i in rooms) {
+				if (rooms[i].ban == true) {
+					continue;
+				}
+				let room = await this.chatRoomsRepository.findOneBy({id: rooms[i].room_id});
+				ret.push({
+					room_id: room.id,
+					name: room.name,
+					access_modifier: room.access_modifier
+				});
+			}
+			return ret;
+		}
+		return null;
+	}
+}
