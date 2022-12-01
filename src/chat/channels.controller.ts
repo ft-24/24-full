f4b5ccd5942fdbfcdb9b560ed200b8850366c4b1
AYoungSn <dudtjs0920@naver.com>
2022-12-01 15:40:12 +0900

    UseGuards 수정

--- src/chat/channels.controller.ts
+++ src/chat/channels.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get, Headers, Param, Res } from "@nestjs/common";
+import { Controller, Get, Headers, Param, Res, UseGuards } from "@nestjs/common";
+import { userInfo } from "os";
+import { JwtAuthGuard } from "src/auth/guard/jwt.guard";
+import { User } from "src/auth/user.decorator";
 import { ChannelService } from "./channels.service";
 
 @Controller('channels')
@@ -6,13 +9,14 @@ export class ChannelsController {
 	constructor(private readonly channelService: ChannelService) {}
 
 	@Get()
-	async getAllChannels(@Headers() headers: any, @Res() res) {
-		
-		return res.status(200).send({rooms: this.channelService.getAllChannels(headers)});
+	@UseGuards(JwtAuthGuard)
+	async getAllChannels(@Res() res, @User() user) {
+		return res.status(200).send({rooms: this.channelService.getAllChannels(user)});
 	}
 
 	@Get(':intra_id')
-	async getParticipateChannels(@Headers() headers: any, @Res() res, @Param('intra_id') intra_id) {
-		return res.status(200).send({rooms: this.channelService.getParticipateChannels(headers, intra_id)})
+	@UseGuards(JwtAuthGuard)
+	async getParticipateChannels(@Res() res, @User() user, @Param('intra_id') intra_id) {
+		return res.status(200).send({rooms: this.channelService.getParticipateChannels(user, intra_id)})
 	}
 }
